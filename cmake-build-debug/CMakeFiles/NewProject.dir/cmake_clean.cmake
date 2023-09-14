5cf9fe68def505071e1446725a112cfa247a8525
m1mist <1664910959@qq.com>
2023-09-14 22:31:39 +0800

    test

---
+++ cmake-build-debug/CMakeFiles/NewProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NewProject.dir/main.cpp.obj"
+  "NewProject.exe"
+  "NewProject.exe.manifest"
+  "NewProject.pdb"
+  "libNewProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NewProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
